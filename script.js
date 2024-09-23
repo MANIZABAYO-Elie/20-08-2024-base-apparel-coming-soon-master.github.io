374808241fb135d605903d473d415256cbe295ba
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-09-23 16:29:50 +0200

    file committed

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
 
-const email = document.getElementById('email');
-
- function verifyEmail(){
-   if (email.trim() ==="") {
-    email.innerText = "please enter a valid email"
+const email = document.getElementById('email').value;
+const email_display= document.getElementById('email_display');
+document.addEventListener('click',function (email) {
+    if (email.trim()==="") {
+        email_display.innerText = "please enter a valid email";
+    }
+})
 
  
-   }
- }
